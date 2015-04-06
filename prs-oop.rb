eafb7211192362c546bcbe3509f689338fcf0693
mikebcapp <mikbcapp@gmail.com>
2015-04-06 14:31:12 +1200

    OOP Rock Paper Scissors game

---
+++ prs-oop.rb
@@ -0,0 +1,113 @@
+# Paper rock scissors is a game involving two players.  Each player chooses either a paper, rock or a pair of scissors. 
+# The hands are compared. Paper beats Rock, Rock beats scissors and scissors beats paper
+class Game
+  CHOICES = {'p' => 'Paper', 'r' => 'Rock', 's' => 'Scissors'}
+
+  attr_reader :player, :computer
+  attr_writer :player_name
+
+  def initialize
+    puts "Please type in your name"
+    player_name = gets.chomp
+    @player = Human.new (player_name)
+    @computer = Computer.new ("C3PO")
+  end
+
+  def play
+    player.pick_hand
+    computer.pick_hand
+   
+    puts player
+    puts computer
+    compare_hands
+  end
+
+  def compare_hands
+    if player.hand == computer.hand
+      puts "Its a tie"
+    elsif player.hand > computer.hand
+      player.hand.display_winning_message
+      puts "#{player.name} won!"
+    else
+      computer.hand.display_winning_message
+      puts "#{computer.name} won!"
+    end
+  end
+end
+
+class Hand
+  include Comparable
+
+  attr_reader :value
+
+  def initialize (v)
+    @value = v
+  end
+
+  def <=> (another_hand)
+    if @value == another_hand.value
+      0
+    elsif (@value == 'r' && another_hand.value == 's') ||
+        (@value == 'p' && another_hand.value == 'r' )||
+        (@value == 's' && another_hand.value == 'p')
+      1
+    else
+      -1
+    end
+  end
+
+  def display_winning_message
+    case @value
+    when 'p'
+      puts 'Paper wraps up rock'
+    when 'r'
+      puts 'Rock smashes scissors'
+    when 's'
+      puts 'Scissors cut paper'
+    end
+  end
+end
+
+class Player
+
+  attr_accessor :hand
+  attr_reader :name
+
+  def initialize (name)
+    @name = name
+  end
+
+  def to_s
+    "#{name} used #{Game::CHOICES[self.hand.value]}"
+  end
+end
+
+class Human < Player
+  def pick_hand
+    begin
+      puts "Pick your choice of (R)ock, (P)aper or (S)cissors"
+      hand_choice = gets.chomp.downcase
+    end until Game::CHOICES.keys.include?(hand_choice)
+
+    self.hand = Hand.new (hand_choice)
+  end
+end
+
+class Computer < Player
+  def pick_hand
+    self.hand = Hand.new(Game::CHOICES.keys.sample)
+  end
+end
+
+puts "-------------------------------"
+puts "Welcome to Paper, Rock Scissors"
+puts "-------------------------------"
+puts
+begin
+  game = Game.new.play
+  puts
+  puts "Do you want to play again? (y/n)"
+  play_again = gets.chomp.downcase
+  puts
+end until play_again == 'n'
+puts "Goodbye"
